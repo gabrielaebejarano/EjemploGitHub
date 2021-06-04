56d04ad0c3c4c9643be19148967983943bdced69
Gabriela Bejarano <gabrielaebejarano@gmail.com>
2021-06-04 20:56:38 -0300

    Modificacion

--- EjemploGitHub/Program.cs
+++ EjemploGitHub/Program.cs
@@ -6,7 +6,7 @@ namespace EjemploGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo!");
         }
     }
 }
